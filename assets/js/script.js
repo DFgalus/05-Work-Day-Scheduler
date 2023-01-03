e2bdff0bf71923b40fa83336077fc6c10e4cba29
Dgalus <damiangalus7@gmail.com>
2023-01-03 15:24:23 -0500

    Fixed bug in html where time block values were negative.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,23 +8,30 @@ $(document).ready(function () {
       // Get nearby values of the description in JQuery
       var text = $(this).siblings(".description").val();
       var time = $(this).parent().attr("id");
+      
 
       // Save text in local storage
       localStorage.setItem(time, text);
   })
+
   function timeTracker() {
     //get current number of hours.
     var timeNow = dayjs().hour();
+    
 
     // loop over time blocks
     $(".time-block").each(function () {
-        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+        var blockTime = parseFloat($(this).attr("id").split("hour")[1]);
+        console.log(blockTime)
+        
+      
 
         // To check the time and add the classes for background indicators
         if (blockTime < timeNow) {
             $(this).removeClass("future");
             $(this).removeClass("present");
             $(this).addClass("past");
+            
         }
         else if (blockTime === timeNow) {
             $(this).removeClass("past");
@@ -35,10 +42,13 @@ $(document).ready(function () {
             $(this).removeClass("present");
             $(this).removeClass("past");
             $(this).addClass("future");
+            
 
         }
     })
 }
+
+//get input from local storage if there is one
 $("#hour-8 .description").val(localStorage.getItem("hour-8"));
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
